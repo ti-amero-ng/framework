47aa7989759e28ceaba3dd92cd80e7ee629974df
-Huang <841683485@qq.com>
2020-03-22 22:21:12 +0800

    调整

--- src/main/java/com/huang/framework/authority/config/WebApplicationConfig.java
+++ src/main/java/com/huang/framework/authority/config/WebApplicationConfig.java
@@ -4,20 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * 定义ViewResolver
  * @author -Huang
  * @create 2020-03-22 15:10
  */
-@Configuration
-@EnableSwagger2
-public class WebApplicationConfig extends WebMvcConfigurationSupport {
-
-    @Override
-    protected void configureViewResolvers(ViewResolverRegistry registry) {
-        registry.viewResolver(new InternalResourceViewResolver());
-    }
-
-}
+//@Configuration
+//public class WebApplicationConfig extends WebMvcConfigurationSupport {
+//
+//    @Override
+//    protected void configureViewResolvers(ViewResolverRegistry registry) {
+//        registry.viewResolver(new InternalResourceViewResolver());
+//    }
+//
+//}
